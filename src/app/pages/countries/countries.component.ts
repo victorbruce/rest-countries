8886da0b081201273c9181df3c07c95efb503237
Victor Bruce <victorbruce82@gmail.com>
2025-07-01 18:14:46 +0000

    feat: test api call for countries and render results

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, inject, signal } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { countryFeature } from '../../store/reducers/country.reducers';
+import * as CountryActions from '../../store/actions/country.actions';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-countries',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './countries.component.html',
-  styleUrl: './countries.component.scss'
+  styleUrl: './countries.component.scss',
 })
 export class CountriesComponent {
+  private readonly store = inject(Store);
 
+  countries = this.store.selectSignal(countryFeature.selectCountries);
+  loading = this.store.selectSignal(countryFeature.selectLoading);
+  error = this.store.selectSignal(countryFeature.selectError);
+
+  constructor() {
+    this.store.dispatch(CountryActions.loadCountries());
+  }
 }
