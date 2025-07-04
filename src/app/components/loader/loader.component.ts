eb6aa9f5725c552a9314889daeb0e20c980ab2ef
Victor Bruce <victorbruce82@gmail.com>
2025-07-04 10:27:04 +0000

    feat: add a loading component

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [CommonModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent {
+  @Input() message: string = '';
+  @Input() size: number = 20;
+}
